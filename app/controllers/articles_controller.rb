1ebd001cc19dd07b68fdd3727815ecb9186889f3
Ume <ume1931@hotmail.com>
2015-09-18 04:46:09 +0500

    my first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,17 @@
+class ArticlesController < ApplicationController
+	def new
+
+	end
+	def create
+		@product="pakistan"
+		redirect_to "/product"
+	end
+	def temp
+		
+	end
+	def update
+
+	end
+	def show
+	end
+end
